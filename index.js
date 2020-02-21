4ee08519ae320af7252b14383ed4a4f96b5ba65a
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2020-02-21 19:38:23 -0300

    Initial

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ var posicao = {"x": 0, "y": 0}
 
 var joguei = false
 
+var jogarBot = null
+
 const game = {
     "new": function(){
+        console.clear()
         tabuleiro = [
                     [-1,-1,-1],
                     [-1,-1,-1],
@@ -30,7 +33,12 @@ const game = {
     },
     "tabuleiro": function(){
         var aux = ""
+        console.log("Jogue usando números, X e Y")
+        console.log("   0    1    2")
         for(let i = 0; i < tabuleiro.length; i++){
+            if(i <= 2){
+                aux += `${i}`
+            }
             for(let b = 0; b < tabuleiro[i].length; b++){
                 if(b === 2){
                     aux += `${vez(parseInt(tabuleiro[i][b]))}\n`
@@ -41,6 +49,20 @@ const game = {
         }
         return aux
     },
+    "botPlay": function(){
+        var idY = Math.floor(Math.random() * tabuleiro.length)
+        var idX = Math.floor(Math.random() * tabuleiro[idY].length)
+        while(tabuleiro[idY][idX] === 0 || tabuleiro[idY][idX] === 1){
+            idY = Math.floor(Math.random() * tabuleiro.length)
+            idX = Math.floor(Math.random() * tabuleiro[idY].length)
+        }
+        tabuleiro[idY][idX] = 0
+        if(quemJoga == 0){
+            quemJoga = 1
+        } else{
+            quemJoga = 0
+        }
+    },
     "play": function(x, y){
         const found = tabuleiro[x][y]
         if(found === -1){
@@ -49,7 +71,12 @@ const game = {
         } else{
             quemJoga = 0
         }
-        tabuleiro[x][y] = quemJoga
+        if(jogarBot !== "y"){
+            tabuleiro[x][y] = quemJoga
+        } else{
+            tabuleiro[x][y] = quemJoga
+            game.botPlay()
+        }
         }
         joguei = false
     },
@@ -92,9 +119,23 @@ const game = {
         }
     }
 }
+
+var rodou = false
+
 //Function perguntar
 function perguntar(){
     console.log(game.tabuleiro())
+    var bot = null
+    if(jogarBot === null){
+    bot = () => {
+        return new Promise((resolve, reject) => {
+        rl.question('Deseja jogar com bot? (y or n)  ', (answer) => {
+            jogarBot = answer
+            resolve()
+        })
+        })
+    }
+    }
     const question1 = () => {
         return new Promise((resolve, reject) => {
         rl.question('Posição X:  ', (answer) => {
@@ -113,17 +154,27 @@ function perguntar(){
         })
     }
     const main = async () => {
+        if(!rodou){
+        await bot()
+        }
         await question1()
         await question2()
+        rodou = true
         if(joguei){
+            if(posicao.x === "" || posicao.x === null){
+                posicao.x = 0
+            }
+            if(posicao.y === "" || posicao.y === null){
+                posicao.y = 0
+            }
             game.play(posicao.x, posicao.y)
             console.clear()
             if(!game.verifica()){
             perguntar()
             } else{
                 console.log(game.verifica())
+                console.log("Iniciando um novo jogo em 2 segundo")
                 setTimeout(() => {
-                    console.log("Iniciando um novo jogo em 2 segundo")
                     game.new()
                     perguntar()
                 }, 2000);
